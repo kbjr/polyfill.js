c847aac2ca6ae2adec0a779fa030fef009ea3e33
James Brumond <kbjr14@gmail.com>
2011-07-08 18:00:19 -0700

    ready for initial deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node start.js
+web: node server/server.js
--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 var
 
+sys = require('sys'),
 url = require('url'),
 http = require('http'),
 path = require('path'),
 
+// Create the http server
 server = http.createServer(function(req, res) {
 
 
 
 });
 
-server.listen(
+// Start listening
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+	sys.puts('Listening on port ' + port);
+});
 
+/* End of file server.js */
