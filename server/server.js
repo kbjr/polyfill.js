180c6beae494acc9b8d7719e5ae2f7cebab08a5b
James Brumond <kbjr14@gmail.com>
2011-07-08 18:01:33 -0700

    ready for initial deploy

--- server/server.js
+++ server/server.js
@@ -7,9 +7,13 @@ path = require('path'),
 
 // Create the http server
 server = http.createServer(function(req, res) {
-
-
-
+	
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	res.write('Server is ready.');
+	res.end();
+	
 });
 
 // Start listening
