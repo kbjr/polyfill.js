76e3da8040ec7cc5a6c860800d7bb3fd04ea6ae6
James Brumond <kbjr14@gmail.com>
2011-07-08 18:13:16 -0700

    basic server structure in place

--- server/server.js
+++ server/server.js
@@ -8,11 +8,34 @@ path = require('path'),
 // Create the http server
 server = http.createServer(function(req, res) {
 	
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.write('Server is ready.');
-	res.end();
+	var urlData = url.parse(req.url);
+	if (urlData.pathname === '') {
+		urlData.pathname = '/';
+	}
+	var uriSegments = urlData.pathname.split('/');
+	
+	switch (uriSegments[1]) {
+		case '':
+			server.ok(res, null, 'Server is ready.');
+		break;
+		// Handle loading the core
+		case 'core':
+			var headers = {
+				'Content-Type': 'application/javascript'
+			};
+			server.ok(res, headers, '/* Loading the polyfill.js core. */');
+		break;
+		// Handle loading polyfills
+		case 'polyfill':
+			var headers = {
+				'Content-Type': 'application/javascript'
+			};
+			server.ok(res, headers, '/* Loading the polyfill.js core. */');
+		break;
+		default:
+			server.notFound(res, null, 'Unknown route ' + urlData.pathname);
+		break;
+	}
 	
 });
 
@@ -21,5 +44,41 @@ var port = process.env.PORT || 3000;
 server.listen(port, function() {
 	sys.puts('Listening on port ' + port);
 });
+
+// ----------------------------------------------------------------------------
+//  Helpers
+
+function createHeadersObject(headers) {
+	var result = {
+		'Content-Type': 'text/plain'
+	};
+	if (headers) {
+		for (var i in headers) {
+			if (headers.hasOwnProperty(i)) {
+				result[i] = headers[i];
+			}
+		}
+	}
+	return result;
+};
+
+// Sends an HTTP response
+server.respond = function(res, status, headers, body) {
+	res.writeHead(status,
+		createHeadersObject(headers)
+	);
+	res.write(body);
+	res.end();
+};
+
+// Sends a 200 OK
+server.ok = function(res, headers, body) {
+	server.respond(res, 200, headers, body);
+};
+
+// Sends a 404 Not Found
+server.notFound = function(res, headers, body) {
+	server.respond(res, 404, headers, body):
+};
 
 /* End of file server.js */
