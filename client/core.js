b0956cf7bdad2697a2fc15fc6f9663b7c6ebfed6
James Brumond <kbjr14@gmail.com>
2011-07-12 19:06:51 -0700

    added new polyfills: eventsource, filter, foreach, indexof, xhr

--- client/core.js
+++ client/core.js
@@ -8,6 +8,96 @@ window.Polyfill = (function() {
 	var nextId = 1;
 	var requests = { };
 	
+	/**
+	 * The list of polyfills
+	 */
+	var polyfills = {
+		
+		// JSON
+		json: {
+			test: function() {
+				return (window.JSON && typeof window.JSON.stringify(1) === 'string');
+			}
+		},
+		
+		// HTML5 localStorage
+		storage: {
+			test: function() {
+				return (!! window.localStorage);
+			},
+			prereqs: ['json']
+		},
+		
+		// HTML5 Input Placeholder Attribute
+		placeholder: {
+			test: function() {
+				var elem = document.createElement('input');
+				elem.type = 'text';
+				var result = ('placeholder' in elem);
+				elem = null;
+				return result;
+			}
+		},
+		
+		// document.querySelectorAll/querySelector
+		querySelectorall: {
+			test: function() {
+				return (!! document.querySelectorAll);
+			}
+		},
+		
+		// window.onhashchange Event
+		hashchange: {
+			test: function() {
+				return ('onhashchange' in window);
+			}
+		},
+		
+		// EventSource
+		eventsource: {
+			test: function() {
+				return ('EventSource' in window);
+			},
+			prereqs: ['xhr']
+		},
+		
+		// XMLHttpRequest
+		xhr: {
+			test: function() {
+				return (!! window.XMLHttpRequest);
+			}
+		},
+		
+		// Array.prototype.forEach
+		foreach: {
+			test: function() {
+				return (!! Array.prototype.forEach);
+			}
+		},
+		
+		// Array.prototype.indexOf
+		indexof: {
+			test: function() {
+				return (!! Array.prototype.indexOf);
+			}
+		},
+		
+		// Array.prototype.filter
+		filter: {
+			test: function() {
+				return (!! Array.prototype.filter);
+			}
+		},
+		
+		// A Fake Polyfill Used In Testing
+		test: {
+			test: function() {
+				return false;
+			}
+		}
+		
+	};
+	
 // ----------------------------------------------------------------------------
 //  External Interface
 	
@@ -33,7 +123,7 @@ window.Polyfill = (function() {
 		}
 		var needed = [ ];
 		while (polys.length) {
-			var polyfill = polyfills[polys.shift()];
+			var polyfill = polyfills[polys.shift().toLowerCase()];
 			if (! polyfill) {
 				throw new Error('No such polyfill "' + test + '"');
 			}
@@ -186,58 +276,8 @@ window.Polyfill = (function() {
 	};
 	
 	/**
-	 * The list of polyfills
+	 * Process the list of polyfills
 	 */
-	var polyfills = {
-		
-		// JSON
-		json: {
-			test: function() {
-				return (window.JSON && typeof window.JSON.stringify(1) === 'string');
-			}
-		},
-		
-		// HTML5 localStorage
-		storage: {
-			test: function() {
-				return (!! window.localStorage);
-			},
-			prereqs: ['json']
-		},
-		
-		// HTML5 Input Placeholder Attribute
-		placeholder: {
-			test: function() {
-				var elem = document.createElement('input');
-				elem.type = 'text';
-				var result = ('placeholder' in elem);
-				elem = null;
-				return result;
-			}
-		},
-		
-		// document.querySelectorAll/querySelector
-		querySelectorall: {
-			test: function() {
-				return (!! document.querySelectorAll);
-			}
-		},
-		
-		// window.onhashchange Event
-		hashchange: {
-			test: function() {
-				return ('onhashchange' in window);
-			}
-		},
-		
-		// A Fake Polyfill Used In Testing
-		test: {
-			test: function() {
-				return false;
-			}
-		}
-		
-	};
 	for (var i in polyfills) {
 		if (polyfills.hasOwnProperty(i)) {
 			polyfills[i] = new PolyfillObject(i, polyfills[i]);
